9ab02cc1b910687a06c7ba0201111e65db9488ba
Narmer Noir Bazile <narmernbazile@gmail.com>
2020-07-16 10:40:01 -0400

    Added emacs configuration files

---
+++ emacs/.emacs.d/elpa/base16-theme-2.2/base16-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "base16-theme" "2.2" "Collection of themes built on combinations of 16 base colors" 'nil :commit "10180e88d6d9434cec367b6c91222dd2fc3bd8ae" :url "https://github.com/belak/base16-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
